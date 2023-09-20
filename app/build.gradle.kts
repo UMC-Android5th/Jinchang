36521453e1063b9ea308f67581d0fd2f174a9506
fprtmjinho <fjrtmjinho@gmail.com>
2023-09-20 14:14:55 +0900

    feat: add first assignment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,9 @@ android {
                 "proguard-rules.pro"
             )
         }
+        buildFeatures{
+            viewBinding = true
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -33,6 +36,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
--- app/src/main/java/com/example/umc_5th/MainActivity.kt
+++ app/src/main/java/com/example/umc_5th/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.umc_5th
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.umc_5th.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBottomNavigation()
+
+    }
+
+    private fun initBottomNavigation(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commit()
+        binding.mainBnv.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    val homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_frm, homeFragment)
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.lookFragment -> {
+                    val lookFragment = LookFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_frm, lookFragment)
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.searchFragment -> {
+                    val searchFragment = SearchFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, searchFragment)
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.lockerFragment -> {
+                    val lockerFragment = LockerFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, lockerFragment)
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
