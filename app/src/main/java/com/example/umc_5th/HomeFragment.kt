ac3a6a446bcf62c79ea65251187681f9ecb07a02
fprtmjinho <fjrtmjinho@gmail.com>
2023-09-22 01:17:34 +0900

    feat: add videoList in HomeFragment.kt

--- app/src/main/java/com/example/umc_5th/HomeFragment.kt
+++ app/src/main/java/com/example/umc_5th/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.umc_5th.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,45 +19,35 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private lateinit var binding: FragmentHomeBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        setTodayMusic();
+        setPotCast();
         // Inflate the layout for this fragment
         return binding.root
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setTodayMusic(){
+        var musicList: Array<Pair<String,String>> = arrayOf(Pair("LILAC","아이유(IU)"),Pair("제목","가수"),Pair("제목","가수"))
+        var musicListAdapter = MusicListAdapter(musicList)
+        binding.homePannelMusicListRv.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = musicListAdapter
+        }
+        musicListAdapter.notifyDataSetChanged()
+    }
+    private fun setPotCast(){
+        var potCastList: Array<Pair<String,String>> = arrayOf(Pair("제목","가수"),Pair("제목","가수"),Pair("제목","가수"))
+        var potCastListAdapter = PotCastListAdapter(potCastList)
+        binding.homePannelPotcastListRv.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = potCastListAdapter
+        }
+        potCastListAdapter.notifyDataSetChanged()
     }
 }
