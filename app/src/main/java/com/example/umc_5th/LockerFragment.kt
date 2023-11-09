e637250648e574938096676cc57b0a1b21b6c84b
fprtmjinho <fjrtmjinho@gmail.com>
2023-11-09 16:07:35 +0900

    feat: LockerFragment.kt 뷰페이져 임시 설정 보관함에 들어갈 목록 데이터를 임시로 생성하는거보다 DB 배우게될때 한번에 짜는게 나을거 같아서 보관함 안만듬

--- app/src/main/java/com/example/umc_5th/LockerFragment.kt
+++ app/src/main/java/com/example/umc_5th/LockerFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.umc_5th.databinding.FragmentLockerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LockerFragment : Fragment() {
     private var _binding: FragmentLockerBinding? = null
     private val binding get() = _binding!!
+    private val information = arrayListOf("저장한 곡", "음악파일")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +19,12 @@ class LockerFragment : Fragment() {
     ): View? {
         _binding = FragmentLockerBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
+//        val lockerAdapter = LockerVPAdapter(this)
+//        binding.lockerContentVp.adapter = lockerAdapter
+        TabLayoutMediator(binding.lockerContentTb, binding.lockerContentVp){
+                tab, position ->
+            tab.text = information[position]
+        }.attach()
         return binding.root
     }
     override fun onDestroyView() {
