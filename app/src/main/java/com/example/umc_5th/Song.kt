a4a453c724c1e588d198623af8079a278cfa415c
fprtmjinho <fjrtmjinho@gmail.com>
2023-11-18 15:58:36 +0900

    feat: Song.kt - Entity, PrimaryKey 추가

--- app/src/main/java/com/example/umc_5th/Song.kt
+++ app/src/main/java/com/example/umc_5th/Song.kt
@@ -1,10 +1,18 @@
 package com.example.umc_5th
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "SongTable")
 data class Song(
     val songName : String = "",
     val singer : String = "",
     var second: Int = 0,
     var playTime: Int = 0,
     var isPlaying: Boolean = false,
-    var music: String = ""
-)
+    var music: String = "",
+    var coverImg: Int? = null,
+    var isLike: Boolean = false
+){
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+}
