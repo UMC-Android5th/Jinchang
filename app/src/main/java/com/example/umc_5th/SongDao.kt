842d5416eeaba93bbf184d62b2cd17f1a2654dbd
fprtmjinho <fjrtmjinho@gmail.com>
2023-11-18 15:59:25 +0900

    feat: SongDao.kt, SongDatabase.kt - SongDB를 사용하기위해 db 정의

---
+++ app/src/main/java/com/example/umc_5th/SongDao.kt
@@ -0,0 +1,21 @@
+package com.example.umc_5th
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SongDao {
+    @Insert
+    fun insert(song:Song)
+    @Update
+    fun update(song:Song)
+    @Delete
+    fun delete(song:Song)
+    @Query("select * from songTable")
+    fun getSongs():List<Song>
+    @Query("select * from songTable where id = :id")
+    fun getSong(id:Int): Song
+}
