c4521d65bd5b3552342c7140c2cde57a3d655a74
fprtmjinho <fjrtmjinho@gmail.com>
2023-11-22 23:30:09 +0900

    feat: SongActivity.kt - setContentView 추가

--- app/src/main/java/com/example/umc_5th/Song.kt
+++ app/src/main/java/com/example/umc_5th/Song.kt
@@ -5,8 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "SongTable")
 data class Song(
-    val songName : String = "",
-    val singer : String = "",
+    var songName : String = "",
+    var singer : String = "",
     var second: Int = 0,
     var playTime: Int = 0,
     var isPlaying: Boolean = false,
--- app/src/main/java/com/example/umc_5th/SongActivity.kt
+++ app/src/main/java/com/example/umc_5th/SongActivity.kt
@@ -22,6 +22,7 @@ class SongActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initPlayList()
         initSong()
         initClickListener()
